42953e4e6d08555b23805442d09ce4c91ed3061c
izzyzizou <izzyzizou@ymail.com>
2018-03-22 18:10:21 -0400

    integrated react

--- src/main/java/com/userinfo/code/UserController.java
+++ src/main/java/com/userinfo/code/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ public class UserController {
 		this.userRepository = userRepository;
 	}
 	@GetMapping("/details")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public List<Users> getUsers(){
 		List<Users> users = this.userRepository.findAll();	
 		return users;
--- src/main/js/src/App.js
+++ src/main/js/src/App.js
@@ -1,35 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Data from './Data';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      image: []
-    }
-  }
-
-  componentDidMount(){
-    let url = 'http://localhost:8080/user/details';
-    fetch(url)
-    .then(response => {
-      if(response.status >= 400){
-        throw new Error("Bad Response");
-      }
-      return response.json();
-    })
-    .then(data => {
-      this.setState({image: data.imageUrl});
-    }).catch(e => {
-      console.log(e);
-    });
-  }
   render() {
     return (
       <div className="App">
-        <div>
-          {this.state.image}
+        <div className="App-header">
+          <Data/>
         </div>
       </div>
     );
